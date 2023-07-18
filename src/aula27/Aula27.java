d9b03bf95a02a8fb945eeaabe37ac6cc783c9970
Guilherme Moraes <127672759+guilhermemoraes1@users.noreply.github.com>
2023-07-17 22:51:34 -0300

    Create Aula27.java

---
+++ src/aula27/Aula27.java
@@ -0,0 +1,19 @@
+package aula27;
+
+public class Aula27 {
+
+	public static void main(String[] args) {
+		double x = 3.0;
+		double y = 4.0;
+		double z = -5.0;
+		double A, B, C;
+		
+		A = Math.sqrt(x);
+		B = Math.pow(y, 2.0);
+		C = Math.abs(z);
+		System.out.println(A);
+		System.out.println(B);
+		System.out.println(C);
+	}
+
+}
