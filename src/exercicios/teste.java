d512141305ec4fa9cb16bd525a133f0554374db5
Guilherme Moraes <127672759+guilhermemoraes1@users.noreply.github.com>
2023-07-19 11:43:54 -0300

    Update teste.java

--- src/exercicios/teste.java
+++ src/exercicios/teste.java
@@ -6,18 +6,16 @@ public class teste {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int A, B, C, D;
-
-		A = sc.nextInt();
-		sc.nextLine();
-		B = sc.nextInt();
-		sc.nextLine();
-		C = sc.nextInt();
-		sc.nextLine();
-		D = sc.nextInt();
-		sc.nextLine();
-		int dif = (A * B - C * D);
-		System.out.println("DIFERENCA = " + dif);
+		int a, b;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		if (a % b == 0 || b % a == 0) {
+			System.out.println("Sao Multiplos");
+		} else {
+			System.out.println("Nao sao Multiplos");
+		}
 
 		sc.close();
 	}
